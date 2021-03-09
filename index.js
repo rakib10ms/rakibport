2ef99849b1d3424252469cfea255f28d982e4a63
Rakib <your_email@example.com>
2021-03-09 15:59:45 +0600

    little updated

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+ $(document).ready(function(){
+    $('.counter').counterUp({
+  delay: 10,
+  time: 1500
+});
+
+  })
+
+
+  $(document).ready(function(){
+
+  $('.custom-alert').click(function(){
+   $(this).fadeOut(800);
+   console.log('clicked')
+ })
+  })
+
+
+
+
+  
+
+//    let button=document.getElementById('form-button');
+//    button.addEventListener('click',function(){
+//        alert("hello");
+//    })
+
+  
+
