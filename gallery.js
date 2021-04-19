845107768e1a8d78539bcb3bc033f759cbeced8d
Rakib <your_email@example.com>
2021-04-20 00:22:22 +0600

    changed

---
+++ gallery.js
@@ -0,0 +1,39 @@
+// init Isotope
+var $grid = $('.grid').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.all-items').on( 'click', 'li', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+
+
+
+  // filter items on active button click
+
+    $('.all-items').on( 'click', 'li', function() {
+        $(this).activeClass('actives').siblings.removeClass('actives');
+     
+
+  });
+  });
+
+
+
+
+
+//   const inputs=document.querySelectorAll('.inp');
+//   const submit=document.querySelector(".submit");
+//   const hiddenMsg=document.querySelector('.hidden');
+
+// //  console.log(submit)
+// //  console.log(hiddenMsg)
+// var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+
+
+//   submit.addEventListener("submit",msg);
+
+
+//   function msg(){
+//     hiddenMsg.classList.add('active');
+//   }
